68eb7b003cc75cb344333ad35cc51e7b196d452e
Eric Kim <eric.kim@streamlyzer.com>
2017-02-27 06:24:03 +0100

    The first drop of Streamlyzer 0.9.5.0

---
+++ appletvos/StreamlyzerPlugin.framework/Headers/StreamlyzerPluginFortvOS.h
@@ -0,0 +1,19 @@
+//
+//  StreamlyzerPluginFortvOS.h
+//  StreamlyzerPluginFortvOS
+//
+//  Created by Eric Kim on 2/10/17.
+//  Copyright © 2017 streamlyzer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StreamlyzerPluginFortvOS.
+FOUNDATION_EXPORT double StreamlyzerPluginFortvOSVersionNumber;
+
+//! Project version string for StreamlyzerPluginFortvOS.
+FOUNDATION_EXPORT const unsigned char StreamlyzerPluginFortvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamlyzerPluginFortvOS/PublicHeader.h>
+
+
